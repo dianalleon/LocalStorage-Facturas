96627cab88f8e7a315b909ad5eab23f7900a9857
dianalleon <diana.kotoko115@gmail.com>
2022-07-27 22:03:09 -0500

    no repetir producto

--- local_factura.js
+++ local_factura.js
@@ -5,19 +5,25 @@ const productos = JSON.parse(localStorage.getItem('producto')) ?? [];
 let tabla = document.querySelector("#table");
 
 //Guardar en el formulario
-
-
-
 var formulario = document.querySelector('#formulario');
-formulario.addEventListener('submit', function () { //recolecta los datos del formulario
+formulario.addEventListener('submit', function (e) { //recolecta los datos del formulario
+  e.preventDefault();
+
   const producto = {
     nombre: document.getElementById('nombre').value,
     cantidad: document.getElementById('cantidad').value,
     precioUnitario: document.getElementById('precioUnitario').value
   }
-  productos.push(producto);
-  localStorage.setItem('producto', JSON.stringify(productos))
-  mostrarTabla()
+
+  let prueb=productos.some((item)=>{
+    return item.nombre==producto.nombre
+  })
+
+if (prueb == false) {
+    productos.push(producto)
+    localStorage.setItem('producto', JSON.stringify(productos));
+    mostrarTabla();
+} else { alert('producto ya existe') }
 });
 
 
@@ -31,9 +37,10 @@ function mostrarTabla() {
           <td>${item.precioUnitario}</td>
     </tr>`
   })
-
-  let tab = document.querySelector("#tab");
-  tab.innerHTML = result;
+    let tab = document.querySelector("#tab");
+    tab.innerHTML = result;
 }
 
-
+function valorTotal(){
+  return 0;
+}
